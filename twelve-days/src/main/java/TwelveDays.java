37de25ffecb8306288c030a882635df1adbb86bc
Neha Agarwal <38171738+NehaAgarwal2598@users.noreply.github.com>
2020-08-07 09:28:57 +0530

    Add files via upload
    Java Solution

---
+++ twelve-days/src/main/java/TwelveDays.java
@@ -0,0 +1,59 @@
+class TwelveDays {
+	String[] sents = {
+   	"Partridge in a Pear Tree", "Turtle Doves", "French Hens", 	"Calling Birds", "Gold Rings", "Geese-a-Laying",
+	"Swans-a-Swimming", "Maids-a-Milking", "Ladies Dancing", "Lords-a-Leaping", "Pipers Piping", "Drummers Drumming"
+    };
+	String[] days = { "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth" };
+
+    String verse(int verseNumber) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    String verses(int startVerse, int endVerse) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+    
+    String sing() {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+}
+
+class TwelveDays {
+    String[] sents = {
+   	"Partridge in a Pear Tree", "Turtle Doves", "French Hens", 	"Calling Birds", "Gold Rings", "Geese-a-Laying",
+	"Swans-a-Swimming", "Maids-a-Milking", "Ladies Dancing", "Lords-a-Leaping", "Pipers Piping", "Drummers Drumming"
+    };
+    String[] day = { "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth" };
+    String[] count = { 
+	"a", "two", "three", "four", "five", "six", "seven", "eight",
+	"nine", "ten", "eleven", "twelve"
+    };
+    String verse(int verseNumber) {
+      	String statement = "On the " + day[verseNumber - 1] + " day of Christmas my true love gave to me: ";
+		statement += count[verseNumber - 1] + " " + sents[verseNumber - 1];
+		for(int i = verseNumber - 1; i >= 2; --i)
+			statement += ", " + count[i - 1] + " " + sents[i - 1];
+
+		if(verseNumber != 1)
+			statement += ", and " + count[0] + " " + sents[0];
+		statement += "./n";
+		statementurn statement;
+    }
+
+    String verses(int startVerse, int endVerse) {
+		String res = "";
+		for(int i = startVerse; i <= endVerse; ++i){
+			res = res + verse(i);
+			if(i != endVerse) res += "/n";
+		}
+		statementurn res;
+    }
+    
+    String sing() {
+    	statementurn verses(1,12);
+    }
+}
+
+C:/Users/shubh/Exercism/java/twelve-days/src/test/java/TwelveDays.java
+C:/Users/shubh/Exercism/java/twelve-days/src/main/java/TwelveDays.java
+exercism submit C:/Users/shubh/Exercism/java/twelve-days/src/test/java/TwelveDays.java C:/Users/shubh/Exercism/java/twelve-days/src/main/java/TwelveDays.java
