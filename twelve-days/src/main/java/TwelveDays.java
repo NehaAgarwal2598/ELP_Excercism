fef47536cc57ee6c3f739897b050cbb4f5626d29
Neha Agarwal <38171738+NehaAgarwal2598@users.noreply.github.com>
2020-08-07 09:43:44 +0530

    Update TwelveDays.java

--- twelve-days/src/main/java/TwelveDays.java
+++ twelve-days/src/main/java/TwelveDays.java
@@ -1,33 +1,35 @@
 class TwelveDays {
-    String[] sents = { "Partridge in a Pear Tree", "Turtle Doves", "French Hens", "Calling Birds", "Gold Rings", "Geese-a-Laying",
-	"Swans-a-Swimming", "Maids-a-Milking", "Ladies Dancing", "Lords-a-Leaping", "Pipers Piping", "Drummers Drumming" };
-	
-    String[] day = { "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth" };
-	
-    String[] count = { "a", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve" };
-	
-    String verse(int verseNumber) {
-      	String statement = "On the " + day[verseNumber - 1] + " day of Christmas my true love gave to me: ";
-		statement += count[verseNumber - 1] + " " + sents[verseNumber - 1];
-		for(int i = verseNumber - 1; i >= 2; --i)
-			statement += ", " + count[i - 1] + " " + sents[i - 1];
+    	String[] sents = { "Partridge in a Pear Tree", "Turtle Doves", "French Hens", 	"Calling Birds", "Gold Rings", "Geese-a-Laying",
+	"Swans-a-Swimming", "Maids-a-Milking", "Ladies Dancing", "Lords-a-Leaping", "Pipers Piping", "Drummers Drumming"};
+    	String[] day = { "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth" };
+	String verse(int verseNumber) 
+	{
+		int i;
+		if(verseNumber==1)
+			return  "On the "+ days[0]+ " day of Christmas my true love gave to me: " +things[0]+".\n";
 
-		if(verseNumber != 1)
-			statement += ", and " + count[0] + " " + sents[0];
-		statement += ".\n";
+		String statement="On the "+ days[verseNumber-1]+ " day of Christmas my true love gave to me: ";
+
+		for(i = verseNumber-1; i > 0; i--)
+			statement+= things[i]+", ";
+
+		statement+= "and "+things[0]+".\n";
 		return statement;
+    	}
+
+    String verses(int startVerse, int endVerse) 
+    {
+        int i;
+	String lyrics="";
+	for(i = startVerse; i < endVerse; i++)
+		lyrics+= verse(i)+ "\n";
+
+	lyrics+= verse(endVerse);
+	return lyrics;
     }
 
-    String verses(int startVerse, int endVerse) {
-		String res = "";
-		for(int i = startVerse; i <= endVerse; ++i){
-			res = res + verse(i);
-			if(i != endVerse) res += "\n";
-		}
-		return res;
-    }
-    
     String sing() {
-    	return verses(1,12);
-    }
+        return verses(1,12);
+     }
 }
+
