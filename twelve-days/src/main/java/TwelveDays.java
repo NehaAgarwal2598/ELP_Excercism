79a91210d0ba954c97044d56ffdb88c69143dfe1
Neha Agarwal <38171738+NehaAgarwal2598@users.noreply.github.com>
2020-08-07 09:33:18 +0530

    Update TwelveDays.java

--- twelve-days/src/main/java/TwelveDays.java
+++ twelve-days/src/main/java/TwelveDays.java
@@ -1,31 +1,11 @@
 class TwelveDays {
-	String[] sents = {
-   	"Partridge in a Pear Tree", "Turtle Doves", "French Hens", 	"Calling Birds", "Gold Rings", "Geese-a-Laying",
-	"Swans-a-Swimming", "Maids-a-Milking", "Ladies Dancing", "Lords-a-Leaping", "Pipers Piping", "Drummers Drumming"
-    };
-	String[] days = { "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth" };
-
-    String verse(int verseNumber) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
-
-    String verses(int startVerse, int endVerse) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
-    
-    String sing() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
-}
-
-class TwelveDays {
-    String[] sents = {
-   	"Partridge in a Pear Tree", "Turtle Doves", "French Hens", 	"Calling Birds", "Gold Rings", "Geese-a-Laying",
-	"Swans-a-Swimming", "Maids-a-Milking", "Ladies Dancing", "Lords-a-Leaping", "Pipers Piping", "Drummers Drumming"
-    };
+    String[] sents = { "Partridge in a Pear Tree", "Turtle Doves", "French Hens", "Calling Birds", "Gold Rings", "Geese-a-Laying",
+	"Swans-a-Swimming", "Maids-a-Milking", "Ladies Dancing", "Lords-a-Leaping", "Pipers Piping", "Drummers Drumming" };
+	
     String[] day = { "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth" };
-    String[] count = { 
-	"a", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve" };
+	
+    String[] count = { "a", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve" };
+	
     String verse(int verseNumber) {
       	String statement = "On the " + day[verseNumber - 1] + " day of Christmas my true love gave to me: ";
 		statement += count[verseNumber - 1] + " " + sents[verseNumber - 1];
@@ -35,7 +15,7 @@ class TwelveDays {
 		if(verseNumber != 1)
 			statement += ", and " + count[0] + " " + sents[0];
 		statement += ".\n";
-		statementurn statement;
+		return statement;
     }
 
     String verses(int startVerse, int endVerse) {
@@ -44,10 +24,10 @@ class TwelveDays {
 			res = res + verse(i);
 			if(i != endVerse) res += "\n";
 		}
-		statementurn res;
+		return res;
     }
     
     String sing() {
-    	statementurn verses(1,12);
+    	return verses(1,12);
     }
 }
